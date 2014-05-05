02423ff0ebda54a1cd662ec6b769d23e2c3b1ba4
sarwesh <sarwesh.krshnn@gmail.com>
2014-05-05 23:46:48 +0530

    until now

--- preloader.js
+++ preloader.js
@@ -24,28 +24,32 @@
 	
     function preloadHandleProgress(event) {
 	   if(event){
-			document.getElementById("center-loading-line").style.borderLeft = (Math.round((event.loaded*100)).toString()*h/100)+"px solid #055";	
-			if(event.loaded*100 > 40 && event.loaded*100 < 60){
-			document.getElementById("center-logo-back").style.borderLeft = ((Math.round(event.loaded*100))*1.67*h1/100)+"px solid #055";
+			document.getElementById("center-loading-line").style.borderLeft = (Math.round((event.loaded*100)).toString()*h/100)+"px solid #CC181E";	
+			if(event.loaded*100 > 45 && event.loaded*100 < 55){
+			document.getElementById("center-loading-box").style.left = (Math.round(event.loaded*100)-45)*10+"%";
 			}
 			if(event.loaded*100 > 60){		//If loading happens too fast
-			document.getElementById("center-logo-back").style.borderLeft= h1+"px solid #055";
+			document.getElementById("center-loading-box").style.left= "0%";
 			}			
 		}
 	}
 
     function preloadHandleComplete(event) {
-		document.getElementById("center-logo-back").style.borderLeft = h1+"px solid #055";
-		
-		// Use preloaded images
-		document.getElementById("container").style.backgroundImage = "URL('images/ironman.jpg')";
-		
-		// Move two windows to the top
+		//If loading happens really fast
+		document.getElementById("center-loading-box").style.marginLeft= "0%";
+		// Move two windows
 		$("#center-loading-line").fadeOut(500,"swing");
-		document.getElementById("window-top").style.top = "-40%";
+		document.getElementById("window-top").style.top = "-50%";
 		document.getElementById("center-logo-back").style.top = "1%";
 		document.getElementById("center-logo").style.top = "3%";
-		document.getElementById("window-bottom").style.top = "90%";
+		document.getElementById("window-bottom").style.top = "100%";
+	
+		// Use preloaded images
+		document.getElementById("container").style.backgroundImage = "URL('images/ironman.jpg')";
+		document.getElementById("container").style.opacity = 1;
+		setTimeout(function(){
+		$("#header").fadeTo(1000,0.9);
+		$("#footer").fadeTo(1000,0.9);},1500);
 	}
 
     init();
